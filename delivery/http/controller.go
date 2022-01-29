e8869774a263f5da3ed5853dd1a44fe6588a15f9
Tiago Rodrigo Lampert <tiagorlampert@gmail.com>
2022-01-29 03:26:19 -0300

    mv middleware package

--- delivery/http/controller.go
+++ delivery/http/controller.go
@@ -4,7 +4,7 @@ import (
 	"github.com/gin-gonic/gin"
 	"github.com/sirupsen/logrus"
 	"github.com/tiagorlampert/CHAOS/internal/environment"
-	"github.com/tiagorlampert/CHAOS/middleware"
+	"github.com/tiagorlampert/CHAOS/internal/middleware"
 	"github.com/tiagorlampert/CHAOS/services"
 )
 

